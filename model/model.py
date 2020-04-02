a066a13a4762196443463a17d07715b14a48a5e0
SJCaldwell <shane.caldwell12@ncf.edu>
2020-04-02 16:02:21 -0500

    Update model.py to take proper shapes

--- model/model.py
+++ model/model.py
@@ -45,15 +45,19 @@ class MyModel(nn.Module):
         )
         self.regressor = nn.Linear((64 * 64 * 12) * 4 + 2, 1) # 4 images smoothed to 64 x 64 x 12, and then our 2 Numbers.
 
-    def forward(self, x1, x2, x3):
+    def forward(self, x1, x2, x3, x4, x5):
         x1 = self.features1(x1)
         x2 = self.features2(x2)
         x3 = self.features3(x3)
+        x4 = self.features(x4)
+        x5 = self.features(x5)
 
         x1 = x1.view(x1.size(0), -1)
         x2 = x2.view(x2.size(0), -1)
         x3 = x3.view(x3.size(0), -1)
+        x4 = x4.view(x4.size(0), -1)
+        x5 = x5.view(x5.size(0), -1)
 
-        x = torch.cat((x1, x2, x3), dim=1)
+        x = torch.cat((x1, x2, x3, x4, x5), dim=1)
         x = self.regressor(x)
         return x
