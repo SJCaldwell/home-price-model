a618d4efe699a3ad3f1b8b27a88f798f9f5103e0
SJCaldwell <shane.caldwell12@ncf.edu>
2020-04-18 16:49:29 -0500

    Some composable models mayhaps

--- model/model.py
+++ model/model.py
@@ -84,8 +84,83 @@ class HouseMLP(nn.Module):
             nn.ReLU(),
             nn.Linear(8, 4),
             nn.ReLU(),
-            nn.Linear(4, 1))
+             nn.Linear(4, 1))
 
     def forward(self, x):
         x = self.mlp(x)
+        return x
+
+# Let's define a module meant to make up a smaller portion of our CNN
+class CNN_Unit(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(CNN_Unit, self).__init__()
+
+        self.conv = nn.Conv2d(in_channels=in_channels, kernel_size=3, out_channels=out_channels)
+        self.bn = nn.BatchNorm2d(num_features=out_channels)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.relu(output)
+
+        return output
+
+class SimpleNet(nn.Module):
+    def __init__(self):
+        super(SimpleNet,self).__init__()
+
+        self.unit1 = CNN_Unit(in_channels=3, out_channels=32)
+        self.unit2 = CNN_Unit(in_channels=32,out_channels=32)
+        self.unit3 = CNN_Unit(in_channels=32,out_channels=32)
+
+        self.pool1 = nn.MaxPool2d(kernel_size=2)
+
+        self.unit4 = CNN_Unit(in_channels=32, out_channels=64)
+        self.unit5 = CNN_Unit(in_channels=64, out_channels=64)
+        self.unit6 = CNN_Unit(in_channels=64, out_channels=64)
+        self.unit7 = CNN_Unit(in_channels=64, out_channels=64)
+
+        self.pool2 = nn.MaxPool2d(kernel_size=2)
+
+        self.unit8 = CNN_Unit(in_channels=64, out_channels=128)
+        self.unit9 = CNN_Unit(in_channels=128, out_channels=128)
+        self.unit10 = CNN_Unit(in_channels=128, out_channels=128)
+        self.unit11 = CNN_Unit(in_channels=128, out_channels=128)
+
+        self.pool3 =  nn.MaxPool2d(kernel_size=2)
+
+        self.unit12 = nn.MaxPool2d(in_channels=128, out_channels=128)
+
+        self.avgpool = nn.AvgPool2d(kernel_size=2)
+
+        self.net = nn.Sequential(self.unit1, self.unit2, self.unit3, self.pool1,
+                                 self.unit4, self.unit5, self.unit6, self.unit7, self.pool2,
+                                 self.unit8, self.unit9, self.unit10, self.unit11, self.pool3,
+                                 self.unit12, self.avgpool)
+    def forward(self, x):
+        x = self.net(x)
+        return x
+
+class HouseMixed(nn.Module):
+    def __init__(self, dropout=0.0):
+        super(HouseMLP, self).__init__()
+        self.mlp = nn.Sequential(
+            nn.Linear(10, 8),
+            nn.ReLU(),
+            nn.Linear(8, 4),
+            nn.ReLU())
+
+        self.cnn = SimpleNet()
+        self.fc1 = nn.Linear(1024, 4)
+        self.relu1 = nn.ReLU()
+        self.regr_head = nn.Linear(4, 1)
+
+    def forward(self, x_img, x_regr):
+        x1 = self.cnn(x_img)
+        x2 = self.mlp(x_regr)
+        x = torch.cat((x1, x2), dim=1)
+        x = self.fc1(x)
+        x = self.ReLU(x)
+        x = self.regr_head(x)
         return x
